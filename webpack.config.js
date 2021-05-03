ae4377f8b487e76265ebf665929e19dafd5e6c7c
monco <moncodp@gmail.com>
2021-05-03 14:33:40 +0300

    init package.json and config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle-[hash].js',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    devServer: {
+        contentBase: 'dist',
+        compress: true,
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+            },
+        }),
+    ],
+};
