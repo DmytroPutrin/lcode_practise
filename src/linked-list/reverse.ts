bcdb349fc03c9c5fc1703122ef26edb3421f5a9e
monco <dmytro.putrin@solarwinds.com>
2021-05-07 06:15:37 +0300

    reverse linked list improve solution

--- src/linked-list/reverse.ts
+++ src/linked-list/reverse.ts
@@ -27,3 +27,23 @@ export function reverseList(head: ListNode | null): ListNode | null {
 
 const list = createList(5);
 console.log(reverseList(list.head));
+
+export function reverseList2(head : ListNode | null): ListNode | null {
+    if (!head || !head.next) {
+        return head;
+    }
+
+    let previous: ListNode | null = null;
+    let current: ListNode | null = head;
+
+    while (current) {
+        const next: ListNode | null = current.next;
+        current.next = previous;
+        [previous, current] = [current, next];
+    }
+
+    return previous;
+}
+
+const list2 = createList(3);
+console.log(1, reverseList2(list2.head));
