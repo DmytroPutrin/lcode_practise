a2a38d4ef3aa0d7ea829932bb2aa6e809f9dbfd9
monco <dmytro.putrin@solarwinds.com>
2021-05-08 11:08:18 +0300

    removeElements.ts add other solution

--- src/linked-list/removeElements.ts
+++ src/linked-list/removeElements.ts
@@ -33,3 +33,29 @@ export function removeElements(head: ListNode | null, val: number): ListNode | n
 const list = createList(6);
 list.addAtIndex(2, 5)
 console.log(removeElements(list.head, 5));
+
+export function removeElements2(head: ListNode | null, val: number): ListNode | null {
+    while (head && head.value === val) {
+        head = head.next;
+    }
+
+    if (!head) {
+        return head;
+    }
+
+    let current = head;
+
+    while (current.next) {
+        if (current.next.value == val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+
+    return head;
+}
+
+const list2 = createList(7);
+list.addAtIndex(0, 2);
+console.log(removeElements(list2.head, 2));
